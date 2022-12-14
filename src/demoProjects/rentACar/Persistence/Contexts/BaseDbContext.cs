acee8b7ebc52afac1417256309a413492f076519
oguzhanoxel <oguzhanoksel@hotmail.com>
2022-12-15 00:29:29 +0300

    added Model entity

--- src/demoProjects/rentACar/Persistence/Contexts/BaseDbContext.cs
+++ src/demoProjects/rentACar/Persistence/Contexts/BaseDbContext.cs
@@ -13,7 +13,8 @@ namespace Persistence.Contexts
 	public class BaseDbContext : DbContext
 	{
 		protected IConfiguration Configuration { get; set; }
-		public DbSet<Brand> SomeFeatureEntities { get; set; }
+		public DbSet<Brand> Brands { get; set; }
+		public DbSet<Model> Models { get; set; }
 
 
 		public BaseDbContext(DbContextOptions dbContextOptions, IConfiguration configuration) : base(dbContextOptions)
@@ -35,17 +36,39 @@ namespace Persistence.Contexts
 				a.ToTable("Brands").HasKey(k => k.Id);
 				a.Property(p => p.Id).HasColumnName("Id");
 				a.Property(p => p.Name).HasColumnName("Name");
+
+				a.HasMany(p => p.Models);
 			});
 
+			modelBuilder.Entity<Model>(a =>
+			{
+				a.ToTable("Models").HasKey(k => k.Id);
+				a.Property(p => p.Id).HasColumnName("Id");
+				a.Property(p => p.Name).HasColumnName("Name");
+				a.Property(p => p.DailyPrice).HasColumnName("DailyPrice");
+				a.Property(p => p.ImageUrl).HasColumnName("ImageUrl");
 
+				a.Property(p => p.BrandId).HasColumnName("BrandId");
+				a.HasOne(p => p.Brand);
+			});
 
 			Brand[] brandSeeds = {
 				new(1, "Bmw"),
 				new(2, "Audi"),
 				new(3, "Ford")
 			};
-			modelBuilder.Entity<Brand>().HasData(brandSeeds);
 
+			Model[] modelSeeds =
+			{
+				new(1, 2, "2003 Audi RS 6", 1234, ""),
+				new(2, 2, "2009 Audi RS 6", 4321, ""),
+				new(3, 1, "2003 M5", 1111, ""),
+				new(4, 1, "2012 M5", 3333, ""),
+				new(5, 3, "2017 Ford Focus RS", 2222, ""),
+			};
+
+			modelBuilder.Entity<Brand>().HasData(brandSeeds);
+			modelBuilder.Entity<Model>().HasData(modelSeeds);
 
 		}
 	}
---
+++ src/demoProjects/rentACar/Persistence/Migrations/20221214212729_AddedModel.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    public partial class AddedModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Models",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BrandId = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DailyPrice = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Models", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Models_Brands_BrandId",
+                        column: x => x.BrandId,
+                        principalTable: "Brands",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Models",
+                columns: new[] { "Id", "BrandId", "DailyPrice", "ImageUrl", "Name" },
+                values: new object[,]
+                {
+                    { 1, 2, 1234m, "", "2003 Audi RS 6" },
+                    { 2, 2, 4321m, "", "2009 Audi RS 6" },
+                    { 3, 1, 1111m, "", "2003 M5" },
+                    { 4, 1, 3333m, "", "2012 M5" },
+                    { 5, 3, 2222m, "", "2017 Ford Focus RS" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Models_BrandId",
+                table: "Models",
+                column: "BrandId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Models");
+        }
+    }
+}
